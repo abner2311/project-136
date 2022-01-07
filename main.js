88b617e58bceb44f3d165de9d47627b19f9b392a
abner2311 <85498429+abner2311@users.noreply.github.com>
2022-01-07 16:55:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+status="";
+video="";
+function preload(){
+    video=createVideo('video.mp4');
+    video.hide();
+}
+function setup(){
+    canvas=createCanvas(480, 380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+    objectDetector=ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML="Status:detecting objects";
+    document.getElementById("number_of_objects").innerHTML="OBJECTS  NOT FOUND";
+}
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
+function start(){
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting objects";
+}
+function modelLoaded(){
+    console.log("Model Loaded !!!");
+status=true;
+video.loop();
+video.speed(1);
+video.volume(0);
+}
